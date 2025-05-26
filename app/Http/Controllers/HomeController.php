29a90af7fdc3bbf2b8463292b146724a2bc03a2a
pirotonta <paulaaliasjev@gmail.com>
2025-05-26 12:12:09 -0300

    homecontroller y auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Post;
+
 class HomeController extends Controller
 {
-    //
+    
+    public function getHome(){
+        $posts = Post::orderBy('post_id', 'desc')->paginate(10);
+        return view('home', compact('posts'));
+    }
+
 }
