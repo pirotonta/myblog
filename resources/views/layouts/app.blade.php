8599c1d9eb4f59dfb435a9cd681c2c420a85fbae
GisellaMoreno7 <gisellamoreno2002@gmail.com>
2025-06-07 22:26:44 -0300

    icono + nombre del blog en el <head>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Bloggit') }}</title>
+    <link rel="icon" href="/icons/forum-icon.svg" type="image/x-icon">
+    <title>@yield('title', 'Bloggit')</title>
 
     <!-- Tailwind -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
